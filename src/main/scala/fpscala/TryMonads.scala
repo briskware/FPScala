9762a1b3413cf2306d4d4d65c84a050676d0861b
Stefan Szaniszlo <stefan@briskware.com>
2015-10-30 02:03:05 +0000

    introduce Failure

--- src/main/scala/fpscala/TryMonads.scala
+++ src/main/scala/fpscala/TryMonads.scala
@@ -1,6 +1,7 @@
-package fpscala.datastructures
+package fpscala
 
 import scala.util.{Failure, Success, Try}
+
 
 /**
  * Created by stefan on 30/10/15.
@@ -12,17 +13,20 @@ object TryMonads extends App {
     case x @ _ => Success(x)
   }
 
-  def g(s: String) = {
-    val result = for {
+  def o[T](arg: T): Option[T] = arg match {
+    case 5 => None
+    case x @ _ => Some(x)
+  }
+
+  def g(ss: Seq[String]) = {
+    for {
+      s <- ss
       step1 <- f(s)
       step2 <- f(step1.length)
       step3 <- f("_" * step2)
     } yield step3
-
-    println(s""""$s" => $result""")
   }
 
-  g("how are you")
-  g("hello")
+  println(g("how are you" :: "hello" :: Nil))
 
 }
